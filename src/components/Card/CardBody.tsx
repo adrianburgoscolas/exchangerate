d9f554be0d9a6f1bddbd1e22c8f5a9839a7c400d
Adrian Burgos <adrian.burgos.colas@gmail.com>
2023-05-05 11:27:27 -0400

    FEATURE: completed frontend mobile view

--- src/components/Card/CardBody.tsx
+++ src/components/Card/CardBody.tsx
@@ -1,23 +1,64 @@
 //Card presentation logic
+import { useState } from 'react'
+
 import CardData from "@/components/Card/CardData"
 
-const sym = ['EUR', 'CAD']
 const result = {
-  "date": "2018-02-22",
-  "historical": "",
-  "info": {
-    "rate": 148.972231,
-    "timestamp": 1519328414
+  "base": "USD",
+  "date": "2021-03-17",
+  "rates": {
+    "EUR": 0.813399,
+    "GBP": 0.72007,
+    "JPY": 107.346001
   },
-  "query": {
-    "amount": 25,
-    "from": "GBP",
-    "to": "JPY"
-  },
-  "result": 3724.305775,
-  "success": true
+  "success": true,
+  "timestamp": 1519296206
 } 
 
 export default function CardBody() {
-  return (<CardData symbols={sym} data={result}/>)
+  const [inputVal, setInputVal] = useState('1')
+  const [to, setTo] = useState(Object.keys(result.rates)[0])
+  const [from, setFrom] = useState(Object.keys(result.rates)[1])
+  const [rate, setRate] = useState({val: 0, currency: ''})
+  const [show, setShow] = useState(false)
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    setShow(true)
+    setRate({val: 0, currency: to})
+    console.log('submit')
+    e.preventDefault()
+  }
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    if(/^\d*$/.test(e.currentTarget.value)) {
+      setInputVal(e.currentTarget.value)
+      setShow(false)
+      console.log('submit number')
+    }
+  }
+  function handleTo(e: React.ChangeEvent<HTMLSelectElement>) {
+    setShow(false)
+    setTo(e.currentTarget.value)
+    console.log('to', e.currentTarget.value)
+  }
+  
+  function handleFrom(e: React.ChangeEvent<HTMLSelectElement>) {
+    setShow(false)
+    setFrom(e.currentTarget.value)
+    console.log('from', e.currentTarget.value)
+  }
+
+  return (<CardData 
+    show={show} 
+    inputVal={inputVal} 
+    to={to} 
+    from={from}
+    handleSubmit={handleSubmit} 
+    handleInput={handleInput} 
+    handleTo={handleTo} 
+    handleFrom={handleFrom} 
+    rate={rate} 
+    symbols={Object.keys(result.rates)} 
+    date={result.date}
+  />)
 }
--- src/components/Card/CardData.tsx
+++ src/components/Card/CardData.tsx
@@ -1,64 +1,22 @@
-import React, { useState} from "react"
+import React from "react"
 
 //Card rendering logic
-type CardSymbols = string[]
-
-
-type CardRate = {
-  "date": string,
-  "historical": string,
-  "info": {
-    "rate": number,
-    "timestamp": number
-  },
-  "query": {
-    "amount": number,
-    "from": string,
-    "to": string
-  },
-  "result": number,
-  "success": boolean
-}
-
-export default function CardData({data, symbols}: {data: CardRate, symbols: CardSymbols}) {
-
-  const [number, setNumber] = useState()
-  const [to, setTo] = useState('')
-  const [from, setFrom] = useState('')
-
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault()
-  }
-
-  function handleInput(e: React.FormEvent) {
-    if(/^\d*$/.test(e.target.value)) {
-      setNumber(e.target.value)
-      console.log('submit number')
-    }
-  }
-  function handleTo(e: React.FormEvent) {
-    setTo(e.target.value)
-    console.log('to', e.target.value)
-  }
-  
-  function handleFrom(e: React.FormEvent) {
-    setFrom(e.target.value)
-    console.log('from', e.target.value)
-  }
+export default function CardData(props: CardData) {
 
   return (
-    <div>
-      <div className='flex gap-4 items-center justify-center py-4'>
-        <p className='text-4xl font-bold text-neutral-300'>{Math.round(data.result * 100) / 100}</p><p className="text-xs">{data.date}</p>
+    <div className="w-80 mx-auto">
+      <div className={`px-6 flex items-start justify-between py-4 ${props.show?'opacity-100':'opacity-0'} transition-all`}>
+        <p className='text-4xl font-bold text-neutral-300'>{props.rate.val}<span className='text-2xl'>{' '}{props.rate.currency}</span></p>
+        <p className="text-neutral-300 text-xs">{props.date}</p>
       </div>
-      <form onSubmit={handleSubmit} className='flex gap-2 items-center w-fit bg-neutral-300 p-2 rounded-md mx-auto text-neutral-600'>
-        <input onChange={handleInput} value={number} required type='text' className='text-sm border border-neutral-400 bg-neutral-300 rounded-md p-1 w-20 text-center focus:outline-none'/>
-        <select onChange={handleFrom} value={from} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
-          {symbols.map((obj: string, i: number) => <option key={i} value={obj}>{obj}</option>)}
+      <form onSubmit={props.handleSubmit} className='flex gap-2 items-center w-fit bg-neutral-300 p-2 rounded-md mx-auto text-neutral-600'>
+        <input onChange={props.handleInput} value={props.inputVal} required type='text' className='text-sm border border-neutral-400 bg-neutral-300 rounded-md p-1 w-20 text-center focus:outline-none'/>
+        <select onChange={props.handleFrom} value={props.from} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
+          {props.symbols.map((obj: string, i: number) => <option key={i} value={obj}>{obj}</option>)}
         </select>
         / 
-        <select onChange={handleTo} value={to} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
-          {symbols.map((obj: string, i: number) => <option key={i} value={obj}>{obj}</option>)}
+        <select onChange={props.handleTo} value={props.to} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
+          {props.symbols.map((obj: string, i: number) => <option key={i} value={obj}>{obj}</option>)}
         </select> 
         <input type="submit" className='text-neutral-500 rounded-md border border-neutral-400 p-1' value='Calc' />
       </form>
