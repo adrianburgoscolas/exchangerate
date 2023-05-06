1e5aa1271574a57e53cd7667c601bea41cd90e0c
Adrian Burgos <adrian.burgos.colas@gmail.com>
2023-05-06 00:38:28 -0400

    FEATURE: added unit test to components

--- src/components/Card/CardData.tsx
+++ src/components/Card/CardData.tsx
@@ -14,16 +14,16 @@ export default function CardData(props: CardData) {
       </div>
       {props.loading?
         <CircularProgress />:
-        <form onSubmit={props.handleSubmit} className='flex gap-2 justify-around items-center w-full bg-neutral-300 p-2 rounded-md mx-auto text-neutral-600'>
-        <input onChange={props.handleInput} value={props.inputVal} required type='text' className='text-sm border border-neutral-400 bg-neutral-300 rounded-md p-1 w-20 text-center focus:outline-none'/>
-          <select onChange={props.handleFrom} value={props.from} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
+        <form role='form' onSubmit={props.handleSubmit} className='flex gap-2 justify-around items-center w-full bg-neutral-300 p-2 rounded-md mx-auto text-neutral-600'>
+          <input role='input' onChange={props.handleInput} value={props.inputVal} required type='text' className='text-sm border border-neutral-400 bg-neutral-300 rounded-md p-1 w-20 text-center focus:outline-none'/>
+          <select role='select' onChange={props.handleFrom} value={props.from} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
           {props.symbols.map((obj: string, i: number) => <option key={i} value={obj}>{obj}</option>)}
           </select>
         / 
-          <select onChange={props.handleTo} value={props.to} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
+          <select role='select' onChange={props.handleTo} value={props.to} className="border border-neutral-400 bg-neutral-300 rounded-md p-1 w-16">
           {props.symbols.map((obj: string, i: number) => <option key={i} value={obj}>{obj}</option>)}
           </select> 
-        <input type="submit" className='text-neutral-500 rounded-md border border-neutral-400 p-1' value='Calc' />
+        <input role='button' type="submit" className='text-neutral-500 rounded-md border border-neutral-400 p-1' value='Calc' />
         </form>
       }
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Card from '@/components/Card'
+import Card from '../components/Card'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
